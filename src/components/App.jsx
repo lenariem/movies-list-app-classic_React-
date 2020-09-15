c70136cf544e16ef9402be74e96f626c07af1612
LenaIT <olena.denysova@gmail.com>
2020-09-15 13:46:51 +0200

    filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,13 +29,15 @@ export default class App extends Component {
         { label: "Inception (advice from Anna)", important: true, like: false, id: randomstring.generate(5) },
         { label: "Tenet (new,cinema)", important: true, like: false, id: randomstring.generate(5) }
       ],
-      term: ''
+      term: '',
+      filter: 'all'
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.onAdd = this.onAdd.bind(this);
     this.onToggleImportant = this.onToggleImportant.bind(this);
     this.onToggleLiked = this.onToggleLiked.bind(this);
     this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
   }
 
   deleteItem(id) {
@@ -105,17 +107,29 @@ export default class App extends Component {
     })
   }
 
+  filterPost(items, filter) {
+    if (filter === 'like') {
+      return items.filter(item => item.like)
+    } else {
+      return items
+    }
+  }
+
   onUpdateSearch(term) {
     this.setState({term})
   }
 
+  onFilterSelect(filter) {
+    this.setState({filter})
+  }
+
   render() {
-    const {data, term} = this.state;
+    const {data, term, filter} = this.state;
     const liked = data.filter(item => item.like).length;
     const watched = data.filter(item => !item.important).length;
     const allPosts = data.length;
     
-    const visiblePosts = this.searchPost(data, term)
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter)
 
     return (
       <div className="app">
@@ -126,7 +140,10 @@ export default class App extends Component {
         />
         <div className="search-panel d-flex">
           <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-          <PostFilter />
+          <PostFilter 
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
         <PostList 
           posts={visiblePosts} 
--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { Button } from "reactstrap";
+import React, { Component } from 'react'
 import "../css/PostFilter.css"
 
-export default function PostFilter() {
-    return (
-        <div className="btn-group">
-            <Button outline color='info'>All</Button>
-            <button type="button" className="btn btn-outline-secondary">Liked</button>
-        </div>
-    )
+/* 
+import { Button } from "reactstrap";
+<Button outline color='info'>All</Button>
+*/
+
+export default class PostFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: 'All'},
+            {name: 'like', label: 'Liked'}
+        ]
+    }
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+            const active = this.props.filter === name;
+            const activeButtonStyle = active ? 'btn-info' : 'btn-outline-secondary'
+
+            return (
+                <button 
+                    key={name} 
+                    type="button" 
+                    className={`btn ${activeButtonStyle}`}
+                    onClick={() => this.props.onFilterSelect(name)}
+                >{label}</button>
+            )
+        })
+
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
 }
